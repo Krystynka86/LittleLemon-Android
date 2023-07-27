32ce9e22c5a9c9b69090423be9a779e6a94fbb09
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-27 14:28:36 +0200

    add custom colors

--- app/src/main/java/com/example/littlelemon_android/ui/theme/Color.kt
+++ app/src/main/java/com/example/littlelemon_android/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object Colors {
+    val PrimaryBlue = Color(0xFF495E57)
+    val PrimaryYellow = Color(0xFFF4CE14)
+
+    val SecondaryOrange = Color(0xFFEE9972)
+    val SecondaryLightBlue = Color(0xFFFBDABB)
+    val SecondaryLightGray = Color(0xFFEDEFEE)
+}
